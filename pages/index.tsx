465038f57378c5cd76f9ca6b834d1e01419fe6ca
Nawed Ali <nawed2611@gmail.com>
2022-11-17 17:01:39 +0530

    Setup Next w/ @solana/web3.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,41 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js'
+import { Keypair } from '@solana/web3.js'
 
 const Home: NextPage = () => {
+  console.log(web3)
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [executable, setExecutable] = useState(false)
+  let keypair = Keypair.generate();
+
+  console.log(keypair);
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+    try {
+
+      setAddress(address);
+      const publicKey = new web3.PublicKey(address);
+      setAddress(publicKey.toBase58());
+
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+
+      connection.getBalance(publicKey).then((balance) => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL);
+      })
+
+      connection.getAccountInfo(publicKey).then((accountInfo: any) => {
+        setExecutable(accountInfo.executable);
+        console.log(accountInfo);
+      })
+    }
+    catch {
+      setAddress('Invalid Address')
+      setBalance(0)
+      alert('Invalid Address')
+    }
   }
 
   return (
@@ -23,6 +50,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Executable: ${executable}`}</p>
       </header>
     </div>
   )
